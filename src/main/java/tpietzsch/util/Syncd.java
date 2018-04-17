48a1faa4a8a635b320fe270eac557be3fb9f77be
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2018-04-17 14:12:24 +0200

    Synchronized access to AffineTransform3D.set()/copy()

---
+++ src/main/java/tpietzsch/util/Syncd.java
@@ -0,0 +1,39 @@
+package tpietzsch.util;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import net.imglib2.realtransform.AffineTransform3D;
+
+public class Syncd<T>
+{
+	private final T t;
+
+	private final BiConsumer< T, T > setter;
+
+	private final Function< T, T > getter;
+
+	public Syncd( T t, BiConsumer< T, T > setter, Function< T, T > getter )
+	{
+		this.t = t;
+		this.setter = setter;
+		this.getter = getter;
+	}
+
+	public synchronized void set( T t )
+	{
+		setter.accept( this.t, t );
+	}
+
+	public synchronized T get()
+	{
+		return getter.apply( this.t );
+	}
+
+	public static Syncd< AffineTransform3D > affine3D()
+	{
+		return new Syncd<>(
+				new AffineTransform3D(),
+				AffineTransform3D::set,
+				AffineTransform3D::copy );
+	}
+}
