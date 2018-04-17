aa061921e54e4e56a7a562ff4b200fe30391d4e7
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2018-04-17 14:14:03 +0200

    Basic framework for setting up easy interactivity (BDV coordinate systems)

---
+++ src/main/java/tpietzsch/day4/Example2.java
@@ -0,0 +1,136 @@
+package tpietzsch.day4;
+
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL3;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import net.imglib2.realtransform.AffineTransform3D;
+import org.joml.Matrix4f;
+import tpietzsch.day2.Shader;
+import tpietzsch.util.MatrixMath;
+import tpietzsch.util.Syncd;
+
+import static com.jogamp.opengl.GL.GL_COLOR_BUFFER_BIT;
+import static com.jogamp.opengl.GL.GL_DEPTH_BUFFER_BIT;
+import static com.jogamp.opengl.GL.GL_DEPTH_TEST;
+import static com.jogamp.opengl.GL.GL_ELEMENT_ARRAY_BUFFER;
+import static com.jogamp.opengl.GL.GL_FLOAT;
+import static com.jogamp.opengl.GL.GL_LINES;
+import static com.jogamp.opengl.GL.GL_UNSIGNED_INT;
+
+/**
+ * Perspective projection looking at BDV window (z=0 plane).
+ * Interactive transform editing.
+ */
+public class Example2 implements GLEventListener
+{
+	private int vao;
+
+	private Shader prog;
+
+	@Override
+	public void init( final GLAutoDrawable drawable )
+	{
+		final GL gl = drawable.getGL();
+		final GL3 gl3 = drawable.getGL().getGL3();
+
+		// ..:: VERTEX BUFFER ::..
+
+		final float vertices[] = {
+				// 3 pos
+				  0.0f,   0.0f, 0.0f,
+				640.0f,   0.0f, 0.0f,
+				640.0f, 480.0f, 0.0f,
+				  0.0f, 480.0f, 0.0f,
+		};
+		final int[] tmp = new int[ 2 ];
+		gl.glGenBuffers( 1, tmp, 0 );
+		final int vbo = tmp[ 0 ];
+		gl.glBindBuffer( GL.GL_ARRAY_BUFFER, vbo );
+		gl.glBufferData( GL.GL_ARRAY_BUFFER, vertices.length * Float.BYTES, FloatBuffer.wrap( vertices ), GL.GL_STATIC_DRAW );
+		gl.glBindBuffer( GL.GL_ARRAY_BUFFER, 0 );
+
+		// ..:: ELEMENT BUFFER ::..
+
+		final int indices[] = {  // note that we start from 0!
+				0, 1,    // first line
+				1, 2,    // second line
+				2, 3,
+				3, 0
+		};
+		gl.glGenBuffers( 1, tmp, 0 );
+		final int ebo = tmp[ 0 ];
+		gl.glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, ebo );
+		gl.glBufferData( GL.GL_ELEMENT_ARRAY_BUFFER, indices.length * Integer.BYTES, IntBuffer.wrap( indices ), GL.GL_STATIC_DRAW );
+		gl.glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, 0 );
+
+		// ..:: SHADERS ::..
+
+		prog = new Shader( gl3, "ex1", "ex1" );
+
+		// ..:: VERTEX ARRAY OBJECT ::..
+
+		gl3.glGenVertexArrays( 1, tmp, 0 );
+		vao = tmp[ 0 ];
+		gl3.glBindVertexArray( vao );
+		gl.glBindBuffer( GL.GL_ARRAY_BUFFER, vbo );
+		gl3.glVertexAttribPointer( 0, 3, GL_FLOAT, false, 3 * Float.BYTES, 0 );
+		gl3.glEnableVertexAttribArray( 0 );
+		gl.glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, ebo );
+		gl3.glBindVertexArray( 0 );
+
+		// ..:: MISC ::..
+
+		gl3.glEnable( GL_DEPTH_TEST );
+	}
+
+	@Override
+	public void dispose( final GLAutoDrawable drawable )
+	{}
+
+	final Syncd< AffineTransform3D > worldToScreen = Syncd.affine3D();
+
+	private final double screenPadding = 100;
+
+	private double dCam = 2000;
+	private double dClip = 1000;
+	private double screenWidth = 640;
+	private double screenHeight = 480;
+
+	@Override
+	public void display( final GLAutoDrawable drawable )
+	{
+		final GL gl = drawable.getGL();
+		final GL3 gl3 = drawable.getGL().getGL3();
+
+		gl.glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
+		gl.glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+		final Matrix4f model = MatrixMath.affine( worldToScreen.get(), new Matrix4f() );
+		final Matrix4f view = new Matrix4f().translate( 0.0f, 0.0f, ( float ) ( -dCam ) );
+		final Matrix4f projection = MatrixMath.screenPerspective( dCam, dClip, screenWidth, screenHeight, screenPadding, new Matrix4f() );
+
+		final Matrix4f pvm = new Matrix4f( projection ).mul( view ).mul( model );
+
+		prog.use( gl3 );
+		prog.setUniform( gl3, "pvm", pvm );
+		gl3.glBindVertexArray( vao );
+		gl3.glDrawElements( GL_LINES, 8, GL_UNSIGNED_INT, 0 );
+	}
+
+	@Override
+	public void reshape( final GLAutoDrawable drawable, final int x, final int y, final int width, final int height )
+	{}
+
+	public static void main( final String[] args ) throws InterruptedException
+	{
+		final InputFrame frame = new InputFrame( "Example2", 640, 480 );
+		Example2 glPainter = new Example2();
+		frame.setGlEventListener( glPainter );
+		frame.setupDefaultTransformHandler( glPainter.worldToScreen::set );
+		frame.show();
+	}
+
+}
---
+++ src/main/java/tpietzsch/day4/InputFrame.java
@@ -0,0 +1,212 @@
+package tpietzsch.day4;
+
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.awt.GLCanvas;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import net.imglib2.realtransform.AffineTransform3D;
+import org.scijava.ui.behaviour.MouseAndKeyHandler;
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
+import org.scijava.ui.behaviour.util.Actions;
+import org.scijava.ui.behaviour.util.Behaviours;
+import org.scijava.ui.behaviour.util.InputActionBindings;
+import org.scijava.ui.behaviour.util.TriggerBehaviourBindings;
+import tpietzsch.util.PainterThread;
+
+public class InputFrame
+{
+	private GLEventListener glEventListener;
+
+	private final PainterThread painterThread;
+
+	private final GLCanvas canvas;
+
+	private long t0;
+
+	private final InputActionBindings keybindings;
+
+	private final TriggerBehaviourBindings triggerbindings;
+
+	private final Actions actions;
+
+	private final Behaviours behaviours;
+
+	private final JFrame frame;
+
+	private final GLEventListener wrapper = new GLEventListener()
+	{
+		@Override
+		public void init( final GLAutoDrawable drawable )
+		{
+			dbgln( "InputFrame.init" );
+			if ( glEventListener != null )
+				glEventListener.init( drawable );
+			else
+				dbgln( "glEventListener == null" );
+		}
+
+		@Override
+		public void dispose( final GLAutoDrawable drawable )
+		{
+			dbgln( "InputFrame.dispose" );
+			if ( glEventListener != null )
+				glEventListener.dispose( drawable );
+			else
+				dbgln( "glEventListener == null" );
+		}
+
+		@Override
+		public void display( final GLAutoDrawable drawable )
+		{
+			dbgln( "InputFrame.display" );
+			if ( glEventListener != null )
+				glEventListener.display( drawable );
+			else
+				dbgln( "glEventListener == null" );
+		}
+
+		@Override
+		public void reshape( final GLAutoDrawable drawable, final int x, final int y, final int width, final int height )
+		{
+			dbgln( "InputFrame.reshape" );
+			dbgln( "x = [" + x + "], y = [" + y + "], width = [" + width + "], height = [" + height + "]" );
+			if ( glEventListener != null )
+				glEventListener.reshape( drawable, x, y, width, height );
+			else
+				dbgln( "glEventListener == null" );
+		}
+	};
+
+	public InputFrame(
+			final String title,
+			final int width,
+			final int height )
+	{
+		final GLCapabilities capsReqUser = new GLCapabilities( GLProfile.getGL2GL3() );
+		canvas = new GLCanvas( capsReqUser );
+		getCanvas().addGLEventListener( wrapper );
+		getCanvas().setPreferredSize( new Dimension( width, height ) );
+
+		painterThread = new PainterThread( getCanvas()::repaint );
+
+		frame = new JFrame( title );
+		frame.getRootPane().setDoubleBuffered( true );
+		frame.getContentPane().add( getCanvas(), BorderLayout.CENTER );
+		frame.pack();
+		frame.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		frame.addWindowListener( new WindowAdapter()
+		{
+			@Override
+			public void windowClosing( final WindowEvent e )
+			{
+				dbgln( "stopping painterThread..." );
+				painterThread.interrupt();
+			}
+		} );
+
+		keybindings = new InputActionBindings();
+		triggerbindings = new TriggerBehaviourBindings();
+
+		final InputTriggerConfig keyConfig = new InputTriggerConfig();
+		actions = new Actions( keyConfig, "all" );
+		actions.install( keybindings, "default" );
+		behaviours = new Behaviours( keyConfig, "all" );
+		behaviours.install( triggerbindings, "default" );
+
+		SwingUtilities.replaceUIActionMap( frame.getRootPane(), getKeybindings().getConcatenatedActionMap() );
+		SwingUtilities.replaceUIInputMap( frame.getRootPane(), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT, getKeybindings().getConcatenatedInputMap() );
+
+		final MouseAndKeyHandler mouseAndKeyHandler = new MouseAndKeyHandler();
+		mouseAndKeyHandler.setInputMap( getTriggerbindings().getConcatenatedInputTriggerMap() );
+		mouseAndKeyHandler.setBehaviourMap( getTriggerbindings().getConcatenatedBehaviourMap() );
+
+		getCanvas().addKeyListener( mouseAndKeyHandler );
+		getCanvas().addMouseListener( mouseAndKeyHandler );
+		getCanvas().addMouseWheelListener( mouseAndKeyHandler );
+		getCanvas().addMouseMotionListener( mouseAndKeyHandler );
+		getCanvas().addFocusListener( mouseAndKeyHandler );
+
+		t0 = -1;
+	}
+
+	public TransformHandler setupDefaultTransformHandler( final TransformHandler.TransformListener listener )
+	{
+		TransformHandler tfHandler = new TransformHandler();
+		tfHandler.install( getDefaultBehaviours() );
+		tfHandler.setCanvasSize( canvas.getWidth(), canvas.getHeight(), false );
+		tfHandler.setTransform( new AffineTransform3D() );
+		tfHandler.listeners().add( listener );
+		tfHandler.listeners().add( t -> requestRepaint() );
+		return tfHandler;
+	}
+
+	public void setGlEventListener( final GLEventListener glEventListener )
+	{
+		this.glEventListener = glEventListener;
+	}
+
+	public void show()
+	{
+		t0 = System.currentTimeMillis();
+		frame.setVisible( true );
+		painterThread.start();
+	}
+
+	public void requestRepaint()
+	{
+		painterThread.requestRepaint();
+	}
+
+	public long dTimeMillis()
+	{
+		return t0 < 0
+				? 0
+				: ( System.currentTimeMillis() - t0 );
+	}
+
+	public InputActionBindings getKeybindings()
+	{
+		return keybindings;
+	}
+
+	public TriggerBehaviourBindings getTriggerbindings()
+	{
+		return triggerbindings;
+	}
+
+	public Actions getDefaultActions()
+	{
+		return actions;
+	}
+
+	public Behaviours getDefaultBehaviours()
+	{
+		return behaviours;
+	}
+
+	public GLCanvas getCanvas()
+	{
+		return canvas;
+	}
+
+	/*
+	 *
+	 *
+	 */
+
+	public static boolean DEBUG = true;
+
+	public static void dbgln( final String string )
+	{
+		if ( DEBUG )
+			System.out.println( string );
+	}
+}
