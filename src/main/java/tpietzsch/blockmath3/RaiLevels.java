b000d913d07249ac73f1c1b8c4ad52be352b0110
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2018-05-12 13:13:30 -0400

    WIP

--- src/main/java/tpietzsch/blockmath3/RaiLevels.java
+++ src/main/java/tpietzsch/blockmath3/RaiLevels.java
@@ -1,10 +1,14 @@
 package tpietzsch.blockmath3;
 
 import java.util.ArrayList;
+import java.util.List;
+import net.imglib2.realtransform.AffineTransform3D;
 
 public class RaiLevels
 {
-	private ArrayList< RaiLevel > raiLevels;
+	private List< RaiLevel > raiLevels;
+
+	private final AffineTransform3D sourceTransform;
 
 	private int timepoint;
 
@@ -13,18 +17,25 @@ public class RaiLevels
 	public RaiLevels()
 	{
 		raiLevels = new ArrayList<>();
+		sourceTransform = new AffineTransform3D();
 	}
 
-	public RaiLevels( final ArrayList< RaiLevel > raiLevels, final int timepoint, final int setup )
+	public RaiLevels( final List< RaiLevel > raiLevels, final AffineTransform3D sourceTransform, final int timepoint, final int setup )
 	{
 		this.raiLevels = raiLevels;
+		this.sourceTransform = sourceTransform;
 		this.timepoint = timepoint;
 		this.setup = setup;
 	}
 
-	public ArrayList< RaiLevel > getRaiLevels()
+	public List< RaiLevel > getRaiLevels()
 	{
 		return raiLevels;
+	}
+
+	public AffineTransform3D getSourceTransform()
+	{
+		return sourceTransform;
 	}
 
 	public int getTimepoint()
@@ -46,6 +57,6 @@ public class RaiLevels
 
 	public RaiLevels copy()
 	{
-		return new RaiLevels( raiLevels, timepoint, setup );
+		return new RaiLevels( raiLevels, sourceTransform, timepoint, setup );
 	}
 }
