d922ec8c341bd642881b54a5fdc5472935444ba2
Ulrik Guenther <guenther@mpi-cbg.de>
2019-05-15 18:31:37 +0200

    Add BufferedSimpleStack3D to support SimpleStack3Ds that are backed directly by a ByteBuffer

--- src/main/java/tpietzsch/example2/DefaultSimpleStackManager.java
+++ src/main/java/tpietzsch/example2/DefaultSimpleStackManager.java
@@ -14,14 +14,14 @@ import net.imglib2.view.Views;
 
 import org.joml.Vector3f;
 
-import tpietzsch.backend.GpuContext;
-import tpietzsch.multires.SimpleStack3D;
+import tpietzsch.backend.*;
+import tpietzsch.multires.*;
 
 public class DefaultSimpleStackManager implements SimpleStackManager
 {
-	private final HashMap< RandomAccessibleInterval< ? >, VolumeTextureU16 > texturesU16;
+	private final HashMap< Object, VolumeTextureU16 > texturesU16;
 
-	private final HashMap< RandomAccessibleInterval< ? >, VolumeTextureU8 > texturesU8;
+	private final HashMap< Object, VolumeTextureU8 > texturesU8;
 
 	public DefaultSimpleStackManager()
 	{
@@ -32,12 +32,47 @@ public class DefaultSimpleStackManager implements SimpleStackManager
 	@Override
 	public SimpleVolume getSimpleVolume( final GpuContext context, final SimpleStack3D< ? > stack )
 	{
-		if ( stack.getType() instanceof UnsignedShortType )
+		if ( stack instanceof BufferedSimpleStack3D ) {
+			final Vector3f sourceMin = new Vector3f();
+			final Vector3f sourceMax = new Vector3f();
+
+			final Texture3D texture;
+			if(stack.getType() instanceof UnsignedByteType) {
+			    final Texture3D existing = texturesU8.get( ((BufferedSimpleStack3D) stack).getBuffer() );
+				if(existing == null) {
+					texture = new VolumeTextureU8();
+					((VolumeTextureU8) texture).init(((BufferedSimpleStack3D) stack).getDimensions());
+					((VolumeTextureU8) texture).upload(context, ((BufferedSimpleStack3D) stack).getBuffer());
+
+					texturesU8.put(((BufferedSimpleStack3D) stack).getBuffer(), (VolumeTextureU8) texture);
+				} else {
+					texture = existing;
+				}
+			}
+			else if ( stack.getType() instanceof UnsignedShortType ) {
+				final Texture3D existing = texturesU16.get( ((BufferedSimpleStack3D) stack).getBuffer() );
+				if(existing == null) {
+					texture = new VolumeTextureU16();
+					((VolumeTextureU16) texture).init(((BufferedSimpleStack3D) stack).getDimensions());
+					((VolumeTextureU16) texture).upload(context, ((BufferedSimpleStack3D) stack).getBuffer());
+
+					texturesU16.put(((BufferedSimpleStack3D) stack).getBuffer(), (VolumeTextureU16) texture);
+				} else {
+					texture = existing;
+				}
+			}
+			else {
+				throw new IllegalArgumentException("Textures of type " + stack.getType() + " are not supported.");
+			}
+
+			return new SimpleVolume( texture, stack.getSourceTransform(), sourceMin, sourceMax );
+		}
+		else if ( stack.getType() instanceof UnsignedShortType )
 		{
 			final RandomAccessibleInterval< ? > image = stack.getImage();
 			final VolumeTextureU16 texture = texturesU16.computeIfAbsent( image, rai -> uploadToTextureU16( context, ( RandomAccessibleInterval< UnsignedShortType > ) rai ) );
-			final Vector3f sourceMax = new Vector3f( image.max( 0 ), image.max( 1 ), image.max( 2 ) );
 			final Vector3f sourceMin = new Vector3f( image.min( 0 ), image.min( 1 ), image.min( 2 ) );
+			final Vector3f sourceMax = new Vector3f( image.max( 0 ), image.max( 1 ), image.max( 2 ) );
 
 			return new SimpleVolume( texture, stack.getSourceTransform(), sourceMin, sourceMax );
 		}
@@ -45,8 +80,8 @@ public class DefaultSimpleStackManager implements SimpleStackManager
 		{
 			final RandomAccessibleInterval< ? > image = stack.getImage();
 			final VolumeTextureU8 texture = texturesU8.computeIfAbsent( image, rai -> uploadToTextureU8( context, ( RandomAccessibleInterval< UnsignedByteType > ) rai ) );
-			final Vector3f sourceMax = new Vector3f( image.max( 0 ), image.max( 1 ), image.max( 2 ) );
 			final Vector3f sourceMin = new Vector3f( image.min( 0 ), image.min( 1 ), image.min( 2 ) );
+			final Vector3f sourceMax = new Vector3f( image.max( 0 ), image.max( 1 ), image.max( 2 ) );
 
 			return new SimpleVolume( texture, stack.getSourceTransform(), sourceMin, sourceMax );
 		}
