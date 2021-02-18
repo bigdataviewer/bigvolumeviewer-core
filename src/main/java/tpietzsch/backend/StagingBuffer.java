30c57391f1ef8513ae92eb2211fd02a2b50b618b
tpietzsch <tobias.pietzsch@gmail.com>
2021-02-18 23:15:25 +0100

    typo

--- src/main/java/tpietzsch/backend/StagingBuffer.java
+++ src/main/java/tpietzsch/backend/StagingBuffer.java
@@ -4,7 +4,7 @@ package tpietzsch.backend;
  * Handle to a staging buffer for texture data. A staging buffer can be
  * {@link GpuContext#map(StagingBuffer) mapped} to obtain a
  * {@link java.nio.Buffer} that can be filled with data. When done writing data,
- * the staging buffer can be {@link GpuContext#unmap(StagingBuffer) unmapping}.
+ * the staging buffer can be {@link GpuContext#unmap(StagingBuffer) unmapped}.
  * Texture blocks at a given offset within the filled staging buffer can be
  * uploaded with
  * {@link GpuContext#texSubImage3D(StagingBuffer, Texture3D, int, int, int, int, int, int, long)}
