224b997ca538dbf966ee932a7a5e1e8e124fa4b1
tpietzsch <tobias.pietzsch@gmail.com>
2022-10-28 13:35:30 +0200

    Bugfix related to https://github.com/scenerygraphics/scenery/issues/493

--- src/main/java/tpietzsch/blockmath/MipmapSizes.java
+++ src/main/java/tpietzsch/blockmath/MipmapSizes.java
@@ -80,13 +80,17 @@ public class MipmapSizes
 		final Matrix4f NDCtoSource = sourceToNDC.invert( new Matrix4f() );
 		final float w = 2f / viewportWidth;
 		// viewport pixel width on near and far plane (in source coordinates)
-		sn = NDCtoSource.transformProject( w, 0, -1, new Vector3f() ).sub( NDCtoSource.transformProject( 0, 0, -1, new Vector3f() ) ).length();
-		sf = NDCtoSource.transformProject( w, 0, 1, new Vector3f() ).sub( NDCtoSource.transformProject( 0, 0, 1, new Vector3f() ) ).length();
+		NDCtoSource.transformProject( 0, 0, -1, pNear );
+		final Vector3f pFar = NDCtoSource.transformProject( 0, 0, 1, new Vector3f() );
+		sn = NDCtoSource.transformProject( w, 0, -1, new Vector3f() ).sub( pNear ).length();
+		sf = NDCtoSource.transformProject( w, 0, 1, new Vector3f() ).sub( pFar ).length();
 
-		NDCtoSource.unprojectInvRay( 0.5f, 0.5f, new int[] { 0, 0, 1, 1 }, pNear, pFarMinusNear );
+		pFar.sub( pNear, pFarMinusNear );
 		pFarMinusNear.normalize( dir );
 		drels = 1f / pFarMinusNear.lengthSquared();
+
 		// voxel size on near plane (in source coordinates)
+		// ... or rather: on any plane perpendicular to dir.
 		v0x = ( float ) Math.sqrt( 1.0 - dir.dot( 1, 0, 0 ) );
 		v0y = ( float ) Math.sqrt( 1.0 - dir.dot( 0, 1, 0 ) );
 		v0z = ( float ) Math.sqrt( 1.0 - dir.dot( 0, 0, 1 ) );
